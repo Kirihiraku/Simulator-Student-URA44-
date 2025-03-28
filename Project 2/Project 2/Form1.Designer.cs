ff404b5818262d26c9f76519348fbaa595e6e3e2
Kirihiraku <vladser.sergeev2017@yandex.ru>
2025-03-28 15:11:06 +0600

    Симулятор студента

---
+++ Project 2/Project 2/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿
+namespace Project_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelEnergy = new System.Windows.Forms.Label();
+            this.labelHunger = new System.Windows.Forms.Label();
+            this.labelHappiness = new System.Windows.Forms.Label();
+            this.labelKnowledge = new System.Windows.Forms.Label();
+            this.labelMoney = new System.Windows.Forms.Label();
+            this.labelSchedule = new System.Windows.Forms.Label();
+            this.labelNotifications = new System.Windows.Forms.Label();
+            this.labelCourse = new System.Windows.Forms.Label();
+            this.progressBarEnergy = new System.Windows.Forms.ProgressBar();
+            this.progressBarHunger = new System.Windows.Forms.ProgressBar();
+            this.progressBarHappiness = new System.Windows.Forms.ProgressBar();
+            this.progressBarKnowledge = new System.Windows.Forms.ProgressBar();
+            this.textBoxMoney = new System.Windows.Forms.TextBox();
+            this.textBoxSchedule = new System.Windows.Forms.TextBox();
+            this.textBoxNotifications = new System.Windows.Forms.TextBox();
+            this.textBoxCourse = new System.Windows.Forms.TextBox();
+            this.buttonStudy = new System.Windows.Forms.Button();
+            this.buttonRelax = new System.Windows.Forms.Button();
+            this.buttonEat = new System.Windows.Forms.Button();
+            this.buttonWork = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelEnergy
+            // 
+            this.labelEnergy.AutoSize = true;
+            this.labelEnergy.Location = new System.Drawing.Point(389, 369);
+            this.labelEnergy.Name = "labelEnergy";
+            this.labelEnergy.Size = new System.Drawing.Size(49, 13);
+            this.labelEnergy.TabIndex = 0;
+            this.labelEnergy.Text = "Энергия";
+            // 
+            // labelHunger
+            // 
+            this.labelHunger.AutoSize = true;
+            this.labelHunger.Location = new System.Drawing.Point(57, 185);
+            this.labelHunger.Name = "labelHunger";
+            this.labelHunger.Size = new System.Drawing.Size(37, 13);
+            this.labelHunger.TabIndex = 1;
+            this.labelHunger.Text = "Голод";
+            // 
+            // labelHappiness
+            // 
+            this.labelHappiness.AutoSize = true;
+            this.labelHappiness.Location = new System.Drawing.Point(389, 318);
+            this.labelHappiness.Name = "labelHappiness";
+            this.labelHappiness.Size = new System.Drawing.Size(48, 13);
+            this.labelHappiness.TabIndex = 2;
+            this.labelHappiness.Text = "Счастье";
+            // 
+            // labelKnowledge
+            // 
+            this.labelKnowledge.AutoSize = true;
+            this.labelKnowledge.Location = new System.Drawing.Point(716, 369);
+            this.labelKnowledge.Name = "labelKnowledge";
+            this.labelKnowledge.Size = new System.Drawing.Size(44, 13);
+            this.labelKnowledge.TabIndex = 3;
+            this.labelKnowledge.Text = "Знания";
+            // 
+            // labelMoney
+            // 
+            this.labelMoney.AutoSize = true;
+            this.labelMoney.Location = new System.Drawing.Point(51, 334);
+            this.labelMoney.Name = "labelMoney";
+            this.labelMoney.Size = new System.Drawing.Size(45, 13);
+            this.labelMoney.TabIndex = 4;
+            this.labelMoney.Text = "Деньги";
+            // 
+            // labelSchedule
+            // 
+            this.labelSchedule.AutoSize = true;
+            this.labelSchedule.Location = new System.Drawing.Point(703, 19);
+            this.labelSchedule.Name = "labelSchedule";
+            this.labelSchedule.Size = new System.Drawing.Size(68, 13);
+            this.labelSchedule.TabIndex = 5;
+            this.labelSchedule.Text = "Расписание";
+            // 
+            // labelNotifications
+            // 
+            this.labelNotifications.AutoSize = true;
+            this.labelNotifications.Location = new System.Drawing.Point(32, 38);
+            this.labelNotifications.Name = "labelNotifications";
+            this.labelNotifications.Size = new System.Drawing.Size(77, 13);
+            this.labelNotifications.TabIndex = 6;
+            this.labelNotifications.Text = "Уведомления";
+            // 
+            // labelCourse
+            // 
+            this.labelCourse.AutoSize = true;
+            this.labelCourse.Location = new System.Drawing.Point(723, 224);
+            this.labelCourse.Name = "labelCourse";
+            this.labelCourse.Size = new System.Drawing.Size(31, 13);
+            this.labelCourse.TabIndex = 7;
+            this.labelCourse.Text = "Курс";
+            // 
+            // progressBarEnergy
+            // 
+            this.progressBarEnergy.Location = new System.Drawing.Point(363, 385);
+            this.progressBarEnergy.Name = "progressBarEnergy";
+            this.progressBarEnergy.Size = new System.Drawing.Size(100, 23);
+            this.progressBarEnergy.TabIndex = 8;
+            // 
+            // progressBarHunger
+            // 
+            this.progressBarHunger.Location = new System.Drawing.Point(22, 201);
+            this.progressBarHunger.Name = "progressBarHunger";
+            this.progressBarHunger.Size = new System.Drawing.Size(100, 23);
+            this.progressBarHunger.TabIndex = 9;
+            // 
+            // progressBarHappiness
+            // 
+            this.progressBarHappiness.Location = new System.Drawing.Point(363, 334);
+            this.progressBarHappiness.Name = "progressBarHappiness";
+            this.progressBarHappiness.Size = new System.Drawing.Size(100, 23);
+            this.progressBarHappiness.TabIndex = 10;
+            // 
+            // progressBarKnowledge
+            // 
+            this.progressBarKnowledge.Location = new System.Drawing.Point(688, 385);
+            this.progressBarKnowledge.Name = "progressBarKnowledge";
+            this.progressBarKnowledge.Size = new System.Drawing.Size(100, 23);
+            this.progressBarKnowledge.TabIndex = 11;
+            // 
+            // textBoxMoney
+            // 
+            this.textBoxMoney.Location = new System.Drawing.Point(22, 350);
+            this.textBoxMoney.Name = "textBoxMoney";
+            this.textBoxMoney.ReadOnly = true;
+            this.textBoxMoney.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMoney.TabIndex = 12;
+            // 
+            // textBoxSchedule
+            // 
+            this.textBoxSchedule.Location = new System.Drawing.Point(688, 35);
+            this.textBoxSchedule.Multiline = true;
+            this.textBoxSchedule.Name = "textBoxSchedule";
+            this.textBoxSchedule.ReadOnly = true;
+            this.textBoxSchedule.Size = new System.Drawing.Size(100, 87);
+            this.textBoxSchedule.TabIndex = 13;
+            // 
+            // textBoxNotifications
+            // 
+            this.textBoxNotifications.Location = new System.Drawing.Point(22, 54);
+            this.textBoxNotifications.Multiline = true;
+            this.textBoxNotifications.Name = "textBoxNotifications";
+            this.textBoxNotifications.ReadOnly = true;
+            this.textBoxNotifications.Size = new System.Drawing.Size(100, 57);
+            this.textBoxNotifications.TabIndex = 14;
+            // 
+            // textBoxCourse
+            // 
+            this.textBoxCourse.Location = new System.Drawing.Point(688, 240);
+            this.textBoxCourse.Name = "textBoxCourse";
+            this.textBoxCourse.ReadOnly = true;
+            this.textBoxCourse.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCourse.TabIndex = 15;
+            // 
+            // buttonStudy
+            // 
+            this.buttonStudy.Location = new System.Drawing.Point(701, 414);
+            this.buttonStudy.Name = "buttonStudy";
+            this.buttonStudy.Size = new System.Drawing.Size(75, 23);
+            this.buttonStudy.TabIndex = 16;
+            this.buttonStudy.Text = "Учиться";
+            this.buttonStudy.UseVisualStyleBackColor = true;
+            this.buttonStudy.Click += new System.EventHandler(this.buttonStudy_Click);
+            // 
+            // buttonRelax
+            // 
+            this.buttonRelax.Location = new System.Drawing.Point(377, 414);
+            this.buttonRelax.Name = "buttonRelax";
+            this.buttonRelax.Size = new System.Drawing.Size(75, 23);
+            this.buttonRelax.TabIndex = 18;
+            this.buttonRelax.Text = "Отдыхать";
+            this.buttonRelax.UseVisualStyleBackColor = true;
+            this.buttonRelax.Click += new System.EventHandler(this.buttonRelax_Click);
+            // 
+            // buttonEat
+            // 
+            this.buttonEat.Location = new System.Drawing.Point(37, 230);
+            this.buttonEat.Name = "buttonEat";
+            this.buttonEat.Size = new System.Drawing.Size(75, 23);
+            this.buttonEat.TabIndex = 20;
+            this.buttonEat.Text = "Поесть";
+            this.buttonEat.UseVisualStyleBackColor = true;
+            this.buttonEat.Click += new System.EventHandler(this.buttonEat_Click);
+            // 
+            // buttonWork
+            // 
+            this.buttonWork.Location = new System.Drawing.Point(35, 415);
+            this.buttonWork.Name = "buttonWork";
+            this.buttonWork.Size = new System.Drawing.Size(75, 23);
+            this.buttonWork.TabIndex = 22;
+            this.buttonWork.Text = "Работать";
+            this.buttonWork.UseVisualStyleBackColor = true;
+            this.buttonWork.Click += new System.EventHandler(this.buttonWork_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Project_2.Properties.Resources.BgB5t3DQLzy_XMkZ2_uIXYC9wPTJM9iq;
+            this.pictureBox1.Location = new System.Drawing.Point(274, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 23;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonWork);
+            this.Controls.Add(this.buttonEat);
+            this.Controls.Add(this.buttonRelax);
+            this.Controls.Add(this.buttonStudy);
+            this.Controls.Add(this.textBoxCourse);
+            this.Controls.Add(this.textBoxNotifications);
+            this.Controls.Add(this.textBoxSchedule);
+            this.Controls.Add(this.textBoxMoney);
+            this.Controls.Add(this.progressBarKnowledge);
+            this.Controls.Add(this.progressBarHappiness);
+            this.Controls.Add(this.progressBarHunger);
+            this.Controls.Add(this.progressBarEnergy);
+            this.Controls.Add(this.labelCourse);
+            this.Controls.Add(this.labelNotifications);
+            this.Controls.Add(this.labelSchedule);
+            this.Controls.Add(this.labelMoney);
+            this.Controls.Add(this.labelKnowledge);
+            this.Controls.Add(this.labelHappiness);
+            this.Controls.Add(this.labelHunger);
+            this.Controls.Add(this.labelEnergy);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelEnergy;
+        private System.Windows.Forms.Label labelHunger;
+        private System.Windows.Forms.Label labelHappiness;
+        private System.Windows.Forms.Label labelKnowledge;
+        private System.Windows.Forms.Label labelMoney;
+        private System.Windows.Forms.Label labelSchedule;
+        private System.Windows.Forms.Label labelNotifications;
+        private System.Windows.Forms.Label labelCourse;
+        private System.Windows.Forms.ProgressBar progressBarEnergy;
+        private System.Windows.Forms.ProgressBar progressBarHunger;
+        private System.Windows.Forms.ProgressBar progressBarHappiness;
+        private System.Windows.Forms.ProgressBar progressBarKnowledge;
+        private System.Windows.Forms.TextBox textBoxMoney;
+        private System.Windows.Forms.TextBox textBoxSchedule;
+        private System.Windows.Forms.TextBox textBoxNotifications;
+        private System.Windows.Forms.TextBox textBoxCourse;
+        private System.Windows.Forms.Button buttonStudy;
+        private System.Windows.Forms.Button buttonRelax;
+        private System.Windows.Forms.Button buttonEat;
+        private System.Windows.Forms.Button buttonWork;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
